138b651fc04715be1c795c5e1efa33843562ec63
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
1999-06-11 04:12:47 +0000

    Initial revision

---
+++ system/unsupervised.c
@@ -0,0 +1,33 @@
+/*====================================================================
+
+		      unsupervised learning 関連
+
+                                             S.Kurohashi 1999. 4. 1
+
+    $Id$
+====================================================================*/
+#include "knp.h"
+
+/* Server Client extention */
+extern FILE  *Infp;
+extern FILE  *Outfp;
+extern int   OptMode;
+
+void CheckCandidates() {
+    int i, j;
+    TOTAL_MGR *tm = &Best_mgr;
+    char buffer[256], buffer2[256];
+
+    /* 各文節ごとにチェック用の feature を与える */
+    for (i = 0; i < Bnst_num; i++)
+	if (tm->dpnd.check[i].num != -1) {
+	    /* 係り側 -> 係り先 */
+	    sprintf(buffer, "Check:%2d %2d", i, tm->dpnd.head[i]);
+	    for (j = 0; j < tm->dpnd.check[i].num; j++) {
+		/* 候補ども */
+		sprintf(buffer2, ":%d", tm->dpnd.check[i].pos[j]);
+		strcat(buffer, buffer2);
+	    }
+	    assign_cfeature(&(bnst_data[i].f), buffer);
+	}
+}
