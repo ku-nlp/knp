2010cfa6f3b0a0f726c15c0571b137344fcdf8a7
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
1999-06-14 00:15:41 +0000

    バッファの長さのチェック。

--- system/unsupervised.c
+++ system/unsupervised.c
@@ -16,7 +16,7 @@ extern int   OptMode;
 void CheckCandidates() {
     int i, j;
     TOTAL_MGR *tm = &Best_mgr;
-    char buffer[256], buffer2[256];
+    char buffer[DATA_LEN], buffer2[256];
 
     /* 各文節ごとにチェック用の feature を与える */
     for (i = 0; i < Bnst_num; i++)
@@ -26,6 +26,10 @@ void CheckCandidates() {
 	    for (j = 0; j < tm->dpnd.check[i].num; j++) {
 		/* 候補ども */
 		sprintf(buffer2, ":%d", tm->dpnd.check[i].pos[j]);
+		if (strlen(buffer)+strlen(buffer2) >= DATA_LEN) {
+		    fprintf(stderr, "Too long string <$s> in CheckCandidates.\n", buffer);
+		    return;
+		}
 		strcat(buffer, buffer2);
 	    }
 	    assign_cfeature(&(bnst_data[i].f), buffer);
