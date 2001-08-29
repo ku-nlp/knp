79683a2a78ed77466787baad0cae31c4b24127de
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2001-08-29 08:53:05 +0000

    *.h の整理。

--- svm/svm.cc
+++ svm/svm.cc
@@ -1,10 +1,52 @@
-#include "common.h"
-#include "misc.h"
-#include "model.h"
-#include "example.h"
-#include "base_example.h"
-#include "kernel.h"
-#include "param.h"
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef STDC_HEADERS
+#include <stdio.h>
+#endif
+
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
+
+#ifdef HAVE_SETJMP_H
+#include <setjmp.h>
+#endif
+
+#ifdef HAVE_MATH_H
+#include <math.h>
+#endif
+
+#ifdef HAVE_SYS_FILE_H
+#include <sys/file.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_CTYPE_H
+#include <ctype.h>
+#endif
+
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+
+#include <tinysvm.h>
 #include "svm.h"
 
 #define	ModelFile	"/home/kawahara/CHUN-LI/knp/system/hoge.model"
@@ -23,11 +65,7 @@ double svm_classify(char *line) {
     int len = strlen(line);
     int i = 0;
 
-    while (isspace (line[i])) i++;
-    /* 
-    y = atof (line + i);
-    while (i < len && !isspace (line[i])) i++;
-    while (i < len && isspace (line[i])) i++; */
+    while (isspace(line[i])) i++;
     dist = model.classify((const char *)(line + i));
 
     return dist;
