f16b948aa1859cf93039e79975ab13aa307ea1bb
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2001-09-05 08:10:56 +0000

    BerkeleyDB 3.X に対応

--- system/db.c
+++ system/db.c
@@ -155,6 +155,197 @@ int db_put(DBM_FILE db, char *buf, char *value, char *Separator, int mode)
 
 #else
 
+/* BerkeleyDB 3 */
+#ifdef DB3
+
+/* DB open for reading */
+DBM_FILE db_read_open(char *filename)
+{
+    DBM_FILE db;
+
+    if ((errno = db_create(&db, NULL, 0))) {
+#ifdef DEBUG
+        fprintf(stderr, "db_read_open: %s: %s\n", filename, (char *)strerror(errno));
+#endif
+	return NULL;
+    }
+
+    /* Initialization */
+    db->set_cachesize(db, 0, 1048576, 0);
+
+    if ((errno = db->open(db, filename, NULL, DB_HASH, DB_RDONLY, 0444))) {
+#ifdef DEBUG
+        fprintf(stderr, "db_read_open: %s: %s\n", filename, (char *)strerror(errno));
+#endif
+	db->close(db, 0);
+	return NULL;
+    }
+    return db;
+}
+
+/* DB open for creating */
+DBM_FILE db_write_open(char *filename)
+{
+    DBM_FILE db;
+
+    if ((errno = db_create(&db, NULL, 0))) {
+#ifdef DEBUG
+        fprintf(stderr, "db_write_open: %s: %s\n", filename, (char *)strerror(errno));
+#endif
+	exit(1);
+    }
+
+    /* Initialization */
+    db->set_cachesize(db, 0, 1048576, 0);
+
+    if ((errno = db->open(db, filename, NULL, DB_HASH, DB_CREATE | DB_TRUNCATE, 0644))) {
+#ifdef DEBUG
+        fprintf(stderr, "db_write_open: %s: %s\n", filename, (char *)strerror(errno));
+#endif
+	db->close(db, 0);
+        exit(1);
+    }
+    return db;
+}
+
+/* DB close */
+void db_close(DBM_FILE db)
+{
+    if ((errno = db->close(db, 0))) {
+	fprintf(stderr, "db->close : %s\n", (char *)strerror(errno));
+        exit(1);
+    }
+}
+
+/* DB list */
+void db_list(DBM_FILE db)
+{
+    DBC *dbc;
+    DBT content, key;
+
+    if ((errno = db->cursor(db, NULL, &dbc, 0))) {
+        fprintf(stderr, "db_list : %s\n", strerror(errno));
+	exit(1);
+    }
+
+    /* Initialization */
+    memset(&key, 0, sizeof(key));
+    memset(&content, 0, sizeof(content));
+
+    while ((errno = dbc->c_get(dbc, &key, &content, DB_NEXT)) == 0) {	
+	fprintf(stdout, "%.*s %.*s\n", (int)key.size, (char *)key.data, 
+		(int)content.size, (char *)content.data);
+    }
+    dbc->c_close(dbc);
+}
+
+/* DB get */
+char *db_get(DBM_FILE db, char *buf)
+{
+    DBT content, key;
+    char *rbuf;
+
+    /* Initialization */
+    memset(&key, 0, sizeof(DBT));
+    memset(&content, 0, sizeof(DBT));
+
+    content.flags = DB_DBT_MALLOC;
+
+    key.data = buf;
+    key.size = strlen(key.data);
+    errno = db->get(db, NULL, &key, &content, 0);
+
+    /* Not existence */
+    if (errno == DB_NOTFOUND)
+	return NULL;
+    /* Other errors */
+    else if (errno) {
+        fprintf(stderr, "db_get: %s (%d)\n", (char *)strerror(errno), errno);
+        exit(1);
+    }
+
+    rbuf = (char *)malloc_data(content.size+1, "db_get");
+    strncpy(rbuf, content.data, content.size);
+    *(rbuf+content.size) = '\0';
+    free(content.data);
+    return rbuf;
+}
+
+/* DB put */
+int db_put(DBM_FILE db, char *buf, char *value, char *Separator, int mode)
+{
+    DBT content, key;
+    char *buffer;
+    int valuesize, i;
+
+    /* Initialization */
+    memset(&key, 0, sizeof(DBT));
+    memset(&content, 0, sizeof(DBT));
+
+    valuesize = strlen(value);
+
+    key.data = buf;
+    key.size = strlen(buf);
+    content.data = value;
+    content.size = valuesize;
+
+    if (mode == DBM_APPEND || mode == DBM_AND || mode == DBM_OR) {
+	errno = db->put(db, NULL, &key, &content, DB_NOOVERWRITE);
+
+	/* key existence */
+	if (errno == DB_KEYEXIST) {
+	    /* get the content which already exists */
+	    errno = db->get(db, NULL, &key, &content, 0);
+
+	    if (mode == DBM_APPEND) {
+		if (Separator)
+		    buffer = (char *)malloc_data(content.size+valuesize+strlen(Separator)+1, "db_put");
+		else
+		    buffer = (char *)malloc_data(content.size+valuesize+1, "db_put");
+		strncpy(buffer, content.data, content.size);
+		buffer[content.size] = '\0';
+		if (Separator)
+		    strcat(buffer, Separator);
+		strcat(buffer, value);
+
+		content.data = buffer;
+		content.size = strlen(content.data);
+	    }
+	    else if (mode == DBM_AND) {
+		for (i = 0; i < content.size; i++)
+		    if (*((char *)(content.data)+i) == '0')
+			value[i] = '0';
+		content.data = value;
+		content.size = valuesize;
+	    }
+	    else if (mode == DBM_OR) {
+		for (i = 0; i < content.size; i++)
+		    if (*((char *)(content.data)+i) == '1')
+			value[i] = '1';
+		content.data = value;
+		content.size = valuesize;
+	    }
+	    errno = db->put(db, NULL, &key, &content, 0);
+	    if (mode == DBM_APPEND)
+		free(buffer);
+	}
+	else if (errno) {
+	    fprintf(stderr, "db_put : %s\n", (char *)strerror(errno));
+	    exit(4);
+	}
+    }
+    else if (mode == DBM_OVERRIDE) {
+	errno = db->put(db, NULL, &key, &content, 0);
+    }
+    else {
+	fprintf(stderr, "db_put : Invalid mode (%d)\n", mode);
+	exit(1);
+    }
+    return 0;
+}
+
+#else
+
 /* DB open for reading */
 DBM_FILE db_read_open(char *filename)
 {
@@ -329,4 +520,5 @@ int db_put(DBM_FILE db, char *buf, char *value, char *Separator, int mode)
     return 0;
 }
 
+#endif
 #endif
