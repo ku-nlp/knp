6000c776e5551edd47ac272bd7cd6bc325083911
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2006-12-09 17:54:35 +0000

    例外タグに関するbug fix.

--- system/case_analysis.c
+++ system/case_analysis.c
@@ -1654,7 +1654,7 @@ void record_case_analysis(SENTENCE_DATA *sp, CF_PRED_MGR *cpm_ptr,
 	    if (cpm_ptr->elem_b_num[num] <= -2 && cpm_ptr->elem_s_ptr[num] == NULL) {
 		sprintf(buffer, "%s/E/%s/-/-/-", 
 			pp_code_to_kstr_in_context(cpm_ptr, cpm_ptr->cmm[0].cf_ptr->pp[i][0]), 
-			ETAG_name[abs(ccp->bnst)]);
+			ETAG_name[1]); /* 不特定-人 */
 		strcat(feature_buffer, buffer);
 	    }
 	    else {
