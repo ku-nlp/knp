d8c0dd48c0d772e58416a60683ad4203f66313ac
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2008-08-18 16:04:58 +0000

    added.

---
+++ perl/lib/KNP/SynGraph.pm
@@ -0,0 +1,175 @@
+# $Id$
+package KNP::SynGraph;
+require 5.004_04; # For base pragma.
+use strict;
+use KNP::SynNode;
+use Encode;
+
+=head1 NAME
+
+KNP::SynGraph - SynGraph in KNP
+
+=head1 DESCRIPTION
+
+SynGraphの各種情報を保持するオブジェクト．
+
+=cut
+sub new {
+    my( $class, $str ) = @_;
+    my $this = {};
+
+    my $midasi_pat = '見出し';
+    if( utf8::is_utf8( $str ) ){
+	$midasi_pat = decode('euc-jp', $midasi_pat);
+    }
+
+    # !! 0 1D <見出し:国際化が><格解析結果:ガ格>
+    my ($tagid, $dpnd, $string) = (split(' ', $str))[1,2,3];
+    my @tagids = split(',', $tagid);
+
+    my ($parent, $dpndtype);
+    if ($dpnd =~ /^([\-\d]+)([DPIA])$/) {
+	$parent = $1;
+	$dpndtype = $2;
+    }
+    else {
+	die "KNP::Tag: Illegal dpnd = $dpnd\n";
+    }
+
+    my ($midasi, @features);
+    while ($string =~ /(<.+?>)/g) {
+	my $s = $1;
+	# 見出し
+	if ($s =~ /<$midasi_pat:(.+?)>/) {
+	    $midasi = $1;
+	}
+	# その他はfeature
+	else {
+	    push @features, $s;
+	}
+    }
+
+    $this->{tagid} = $tagid;
+    $this->{tagids} = \@tagids;
+    $this->{parent} = $parent;
+    $this->{dpndtype} = $dpndtype;
+    $this->{midasi} = $midasi;
+    $this->{feature} = join('', @features);
+
+    bless $this, $class;
+}
+
+=head1 METHODS
+
+以下のメソッドが利用可能である。
+
+=over 4
+
+=item push_synnode
+
+Synノードを追加する
+
+=cut
+sub push_synnode {
+    my ($this, $str) = @_;
+
+    my $synnode = KNP::SynNode->new($str);
+
+    push @{$this->{synnodes}}, $synnode;
+}
+
+=item synnode
+
+全てのSynノードを返す
+
+=cut
+sub synnode {
+    my ($this) = @_;
+
+    if( defined $this->{synnodes} ){
+	@{$this->{synnodes}};
+    } else {
+	wantarray ? () : 0;
+    }
+}
+
+=item tagid
+
+対応する基本句IDを返す。
+
+=cut
+sub tagid {
+    my ($this) = @_;
+    $this->{tagid};
+}
+
+=item tagids
+
+対応する基本句ID(配列)を返す。
+
+=cut
+sub tagids {
+    my ($this) = @_;
+    @{$this->{tagids}};
+}
+
+=item parent
+
+係り先の基本句IDを返す。
+
+=cut
+sub parent {
+    my ($this) = @_;
+    $this->{parent};
+}
+
+=item dpndtype
+
+依存関係の種類(D,P,I,A)を返す。
+
+=cut
+sub dpndtype {
+    my ($this) = @_;
+    $this->{dpndtype};
+}
+
+=item midasi
+
+見出しを返す。
+
+=cut
+sub midasi {
+    my ($this) = @_;
+    $this->{midasi};
+}
+
+=item feature
+
+文法素性を返す。
+
+=cut
+sub feature {
+    my ($this) = @_;
+    $this->{feature};
+}
+
+=back
+
+=head1 AUTHOR
+
+=over 4
+
+=item
+柴田 知秀 <shibata@nlp.kuee.kyoto-u.ac.jp>
+
+=cut
+
+1;
+__END__
+# Local Variables:
+# mode: perl
+# coding: euc-japan
+# use-kuten-for-period: nil
+# use-touten-for-comma: nil
+# End:
+
---
+++ perl/lib/KNP/SynNode.pm
@@ -0,0 +1,130 @@
+# $Id$
+package KNP::SynNode;
+require 5.004_04; # For base pragma.
+use strict;
+use Encode;
+
+=head1 NAME
+
+KNP::SynNode - SynNode in KNP
+
+=head1 DESCRIPTION
+
+SynNodeの各種情報を保持するオブジェクト．
+
+=cut
+sub new {
+    my( $class, $str ) = @_;
+    my $this = {};
+
+    my $score_pat = 'スコア';
+    if( utf8::is_utf8( $str ) ){
+	$score_pat = decode('euc-jp', $score_pat);
+    }
+
+    # ! 1 <SYNID:近い/ちかい><スコア:1>
+    # ! 1 <SYNID:s199:親しい/したしい><スコア:0.99>
+    # ! 1 <SYNID:s1201:所在/しょざい><スコア:0.693><上位語><下位語数:323>
+    my ($tagid, $string) = (split(' ', $str))[1,2];
+    my @tagids = split(',', $tagid);
+
+    my ($synid, $score, @features);
+    while ($string =~ /(<.+?>)/g) {
+	my $s = $1;
+	# SYNID
+	if ($s =~ /<SYNID:(.+?)>/) {
+	    $synid = $1;
+	}
+	elsif ($s =~ /<$score_pat:(.+?)>/) {
+	    $score = $1;
+	}
+	# その他はfeature
+	else {
+	    push @features, $s;
+	}
+    }
+
+    $this->{synid} = $synid;
+    $this->{tagid} = $tagid;
+    $this->{tagids} = \@tagids;
+    $this->{score} = $score;
+    $this->{feature} = join('', @features);
+
+    bless $this, $class;
+}
+
+=head1 METHODS
+
+以下のメソッドが利用可能である。
+
+=over 4
+
+=item tagid
+
+対応する基本句IDを返す。
+
+=cut
+sub tagid {
+    my ($this) = @_;
+    $this->{tagid};
+}
+
+=item tagids
+
+対応する基本句ID(配列)を返す。
+
+=cut
+sub tagids {
+    my ($this) = @_;
+    @{$this->{tagids}};
+}
+
+=item synid
+
+SynIDを返す。
+
+=cut
+sub synid {
+    my ($this) = @_;
+    $this->{synid};
+}
+
+=item synid
+
+スコアを返す。
+
+=cut
+sub score {
+    my ($this) = @_;
+    $this->{score};
+}
+
+=item feature
+
+文法素性を返す。
+
+=cut
+sub feature {
+    my ($this) = @_;
+    $this->{feature};
+}
+
+=back
+
+=head1 AUTHOR
+
+=over 4
+
+=item
+柴田 知秀 <shibata@nlp.kuee.kyoto-u.ac.jp>
+
+=cut
+
+1;
+__END__
+# Local Variables:
+# mode: perl
+# coding: euc-japan
+# use-kuten-for-period: nil
+# use-touten-for-comma: nil
+# End:
