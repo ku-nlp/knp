e8189987cd09acc86dec71752531993e86fe2935
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2009-10-19 23:24:32 +0000

    deleted.

--- svm_light/svm.h
+++
@@ -1,4 +0,0 @@
-extern int init_svm_for_anaphora();
-extern double svm_classify_for_anaphora(char *line, int pp);
-extern int init_svm_for_NE();
-extern double svm_classify_for_NE(char *line, int pp);
