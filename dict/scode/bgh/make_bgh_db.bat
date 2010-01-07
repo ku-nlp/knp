966dad9574dcebad7c76a3cd737533feb54c6d2d
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2010-01-07 00:34:37 +0000

    Delete db files before creation.

--- dict/scode/bgh/make_bgh_db.bat
+++ dict/scode/bgh/make_bgh_db.bat
@@ -3,8 +3,9 @@ echo off
 if not exist bunruidb.txt goto error
 
 perl bgh.pl < bunruidb.txt > bgh.dat
+del /F bgh.db
 make_db bgh.db < bgh.dat
-del bgh.dat
+del /F sm2code.db
 make_db sm2code.db < sm2code.dat
 
 echo 分類語彙表データベースが作成されました。
