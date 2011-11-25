55506a384edfe5a5fa0bd5af0a3706ed6a754950
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2011-11-25 07:40:22 +0000

    Changed -wikipedia, -fallback-to-dpnd, -cf-cache to be default.

--- system/main.c
+++ system/main.c
@@ -221,7 +221,7 @@ extern int	EX_match_subject;
     OptDiscPredMethod = OPT_NORMAL;
     OptDiscNounMethod = OPT_NORMAL;
     OptLearn = FALSE;
-    OptCaseFlag = OPT_CASE_USE_REP_CF | OPT_CASE_USE_CREP_CF | OPT_CASE_USE_CN_CF | OPT_CASE_USE_PROBABILITY | OPT_CASE_ADD_SOTO_WORDS | OPT_CASE_GENERALIZE_AGENT | OPT_CASE_FIX_CF_SEARCH | OPT_CASE_CLEAR_CF | OPT_CASE_USE_CV_CF | OPT_CASE_CF_USE_ID;
+    OptCaseFlag = OPT_CASE_USE_REP_CF | OPT_CASE_USE_CREP_CF | OPT_CASE_USE_CN_CF | OPT_CASE_USE_PROBABILITY | OPT_CASE_ADD_SOTO_WORDS | OPT_CASE_GENERALIZE_AGENT | OPT_CASE_FIX_CF_SEARCH | OPT_CASE_CLEAR_CF | OPT_CASE_USE_CV_CF | OPT_CASE_CF_USE_ID | OPT_CASE_FALLBACK_TO_DPND | OPT_CASE_CF_CACHE;
     OptDiscFlag = 0;
     OptServerFlag = 0;
     OptIgnoreChar = '\0';
@@ -307,8 +307,8 @@ extern int	EX_match_subject;
 	    OptAnalysis = OPT_DPND;
 	    OptUseCF = FALSE;
 	}
-	else if (str_eq(argv[0], "-fallback-to-dpnd")) {
-	    OptCaseFlag |= OPT_CASE_FALLBACK_TO_DPND;
+	else if (str_eq(argv[0], "-no-fallback-to-dpnd")) {
+	    OptCaseFlag &= ~OPT_CASE_FALLBACK_TO_DPND;
 	}
 	else if (str_eq(argv[0], "-filter")) {
 	    OptAnalysis = OPT_FILTER;
@@ -819,8 +819,8 @@ extern int	EX_match_subject;
 	else if (str_eq(argv[0], "-no-clear-cf")) {
 	    OptCaseFlag &= ~OPT_CASE_CLEAR_CF;
 	}
-	else if (str_eq(argv[0], "-cf-cache")) {
-	    OptCaseFlag |= OPT_CASE_CF_CACHE;
+	else if (str_eq(argv[0], "-no-cf-cache")) {
+	    OptCaseFlag &= ~OPT_CASE_CF_CACHE;
 	}
 #ifdef CDB
 	else if (str_eq(argv[0], "-cf-on-memory")) {
@@ -988,7 +988,7 @@ extern int	EX_match_subject;
 	else if (str_eq(argv[0], "-disable-emoticon-recognition")) { /* used in rules */
 	    ;
 	}
-	else if (str_eq(argv[0], "-wikipedia")) { /* used in rules */
+	else if (str_eq(argv[0], "-no-wikipedia")) { /* used in rules */
 	    ;
 	}
 	else {
